6506f7591933b6e27e395071f5a08edfcdd8c49c
runabane <santaklausmann@gmail.com>
2024-10-04 13:18:00 +0200

    added scrapper

---
+++ backend/src/main/java/io/swagger/entity/ScrappedFilm.java
@@ -0,0 +1,11 @@
+package io.swagger.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class ScrappedFilm {
+    @Id
+    private String id;
+    poprivate st
+}
